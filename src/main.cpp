1ecdfd6bf16f016f09fb57832e7ec2de600ffe46
Jonathan Ludwig <jr.ludwig@gmail.com>
2021-12-18 21:53:03 -0700

    Add switch between normal and demo mode

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,10 @@
 #define CHANNEL_0 0
 
 #define SENSOR_INTERRUPT_PIN    10
+#define BUTTON_PIN              7
+
+#define NORMAL_BLINK_INTERVAL_MS    2000
+#define DEMO_BLINK_INTERVAL_MS      200
 
 // put in include/ant_network_key.h:
 // const uint8_t NETWORK_KEY[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77}; // get this from thisisant.com
@@ -27,7 +31,7 @@
 
 ArduinoNativeAntWithCallbacks ant;
 AntPlusRouter router;
-ProfileBicyclePowerSensor bikePower(123, 0, ANTPLUS_BICYCLEPOWER_FLAGS_SENSORTYPE_TORQUECRANK);
+ProfileBicyclePowerSensor bikePower(123, 0, ANTPLUS_BICYCLEPOWER_FLAGS_SENSORTYPE_POWERONLY);
 
 void batteryStatusDataPageHandler(BatteryStatusMsg& msg, uintptr_t data);
 void manufacturerIDDataPageHandler(ManufacturersInformationMsg& msg, uintptr_t data);
@@ -103,15 +107,19 @@ public:
         auto elapsed_time = local_interrupt_data.event_time - _last__interrupt_data.event_time;
         // Serial.printf("revs=%u, ms=%u\n", revolution_count, elapsed_time);
 
-        auto revoltuions_per_second = static_cast<double>(1000 * revolution_count) / static_cast<double>(elapsed_time);
-        // Serial.printf("RPS=%.4f\n", revoltuions_per_second);
+        auto revoltuions_per_second = static_cast<double>(1000000 * revolution_count) / static_cast<double>(elapsed_time);
+        Serial.printf("RPS=%.4f\n", revoltuions_per_second);
 
         _last__interrupt_data.interrupt_data = local_interrupt_data.interrupt_data;
 
         auto power_watts = 0.0486 * pow(revoltuions_per_second, 1.86);
-        // Serial.printf("PWR=%.4f\n", power_watts);
+        Serial.printf("PWR=%.4f watts\n", power_watts);
 
         return static_cast<uint16_t>(power_watts);
+    }
+    void simulateInterrupt()
+    {
+        on_pin_interrupt();
     }
 protected:
     static void pin_interrupt()
@@ -120,12 +128,16 @@ protected:
     }
     void on_pin_interrupt()
     {
-        volatile InterruptData local_interrupt_data;
-        local_interrupt_data.interrupt_data = _interrupt_data.interrupt_data;
-        local_interrupt_data.revolution_count = _interrupt_data.interrupt_data + 1;
-        local_interrupt_data.event_time = millis();
-        _interrupt_data.interrupt_data = local_interrupt_data.interrupt_data;
-        _received_first_interrupt = true;
+        uint32_t now = micros();
+        if (now - _interrupt_data.event_time > 3000)   // Debounce the interrupt. This is 91.44 kmh. Seems like a reasonable cap.
+        {
+            volatile InterruptData local_interrupt_data;
+            local_interrupt_data.interrupt_data = _interrupt_data.interrupt_data;
+            local_interrupt_data.revolution_count = _interrupt_data.interrupt_data + 1;
+            local_interrupt_data.event_time = now;
+            _interrupt_data.interrupt_data = local_interrupt_data.interrupt_data;
+            _received_first_interrupt = true;
+        }
     }
 private:
     volatile InterruptData _interrupt_data{};
@@ -134,14 +146,29 @@ private:
     mutable bool _handled_first_request = false;
 };
 
+bool simulate_wheel_interrupt = false;
+volatile uint32_t blink_interval_ms = NORMAL_BLINK_INTERVAL_MS;
 
+void button_interrupt()
+{
+    simulate_wheel_interrupt = !simulate_wheel_interrupt;
+    if (simulate_wheel_interrupt)
+    {
+        blink_interval_ms = DEMO_BLINK_INTERVAL_MS;
+    }
+    else
+    {
+        blink_interval_ms = NORMAL_BLINK_INTERVAL_MS;
+    }
+}
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
     Serial.begin(BAUD_RATE);
-    flash(2);
-    Serial.println("Running");
+    delay(100);
+    Serial.println("Running ...");
+    flash(3, 200);
 
     Bluefruit.begin();
 
@@ -160,16 +187,44 @@ void setup() {
 
     WheelSensor::instance().begin(SENSOR_INTERRUPT_PIN);
 
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    attachInterrupt(BUTTON_PIN, button_interrupt, FALLING);
+
     Serial.println("Running...");
-    flash(3);
+    flash(3, 200);
+
+    // simulate_wheel_interrupt = true;
 }
+
+const uint32_t interruptIntervalMs = 20;    // Reports ~70 watts
+volatile uint32_t lastInterruptTime = 0;
+
+volatile uint32_t last_blink_time = 0;
 
 void loop() {
     // call this frequently
     router.loop();
+    auto now = millis();
+    if (simulate_wheel_interrupt)
+    {
+        auto elapsedMs = now - lastInterruptTime;
+        if (elapsedMs >= interruptIntervalMs)
+        {
+            WheelSensor::instance().simulateInterrupt();
+            lastInterruptTime = now;
+            // Serial.println(elapsedMs);
+        }
+    }
+    if (now - last_blink_time >= blink_interval_ms)
+    {
+        digitalToggle(LED_BUILTIN);
+        last_blink_time = now;
+    }
 }
 
 void printDpMsg(int dp, const char* s) {
+    Serial.print(millis());
+    Serial.print(": ");
     Serial.print("Sending DataPage: ");
     Serial.print(dp);
     Serial.print(" - ");
@@ -192,10 +247,14 @@ void productIDDataPageHandler(ProductInformationMsg& msg, uintptr_t data) {
 
 void powerOnlyDataPageHandler(BicyclePowerStandardPowerOnlyMsg& msg, uintptr_t data) {
     static uint8_t eventCount = 0;
+    static uint16_t accumulated_power = 0;
 
     printDpMsg(16, "Power Only");
 
-    msg.setUpdateEventCount(eventCount++);
-    msg.setCumulativePowerCount(eventCount * 3);
-    msg.setInstantaneousPowerCount(WheelSensor::instance().power_watts());
+    auto power_watts = WheelSensor::instance().power_watts();
+    accumulated_power += power_watts;
+
+    msg.setUpdateEventCount(++eventCount);
+    msg.setCumulativePowerCount(accumulated_power);
+    msg.setInstantaneousPowerCount(power_watts);
 }
